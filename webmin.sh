5d1625612c0d0ba2fca22cc912b160466cb5e25a
DeleonDuncan <47506607+DeleonDuncan@users.noreply.github.com>
2019-03-17 18:03:52 +0100

    Create webmin.sh

---
+++ webmin.sh
@@ -0,0 +1,12 @@
+#Installation of Webmin
+
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+
+wget http://www.webmin.com/jcameron-key.asc
+sudo apt-key add jcameron-key.asc
+
+sudo apt update
+
+sudo apt install webmin
+
+echo "Webmin installed successfully!"
